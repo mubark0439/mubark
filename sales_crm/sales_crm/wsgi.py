1bbe58226db11fa6408260a7c0872b2049621263
mubarak <162392226+mbark331122@users.noreply.github.com>
2025-06-22 02:01:49 +0300

    إصلاح render.yaml وتحديث المتطلبات

--- sales_crm/sales_crm/wsgi.py
+++ sales_crm/sales_crm/wsgi.py
@@ -15,5 +15,11 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_crm.settings")
+path = '/home/mbark331122/mubark'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sales_crm.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
