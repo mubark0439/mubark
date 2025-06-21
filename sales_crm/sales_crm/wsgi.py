09303833092f00145adceb8c283fc69bde2665d7
mubarak <162392226+mbark331122@users.noreply.github.com>
2025-06-21 08:21:52 +0300

    تحديث إعدادات النشر: ضبط ALLOWED_HOSTS وإعدادات Render

--- sales_crm/sales_crm/wsgi.py
+++ sales_crm/sales_crm/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sales_crm.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_crm.settings")
 application = get_wsgi_application()
